83bc6c22597418bb2af419689d3c7d194fa83d4f
Malix <alixbrunetcontact@gmail.com>
2024-03-01 11:06:38 +0000

    feat: base

--- src/main.go
+++ src/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	// standard
+	"log/slog"
+	"os"
+
+	// external
+	"github.com/disgoorg/disgo"
+)
+
+var (
+	TOKEN         = os.Getenv("TOKEN")
+	CLIENT_SECRET = os.Getenv("CLIENT_SECRET")
+)
+
+func main() {
+	slog.Info("Start")
+	slog.Info("DisGo", slog.String("version", disgo.Version))
+
+	disgo.New(
+		TOKEN,
+	)
+}
